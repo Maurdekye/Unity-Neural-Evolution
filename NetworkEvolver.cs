c590fbd3300bac82ca1cca22fed8fa5d785a69a8
Maurdekye <ncolaprete@gmail.com>
2018-06-09 20:07:11 -0400

    Initial Commit

---
+++ NetworkEvolver.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class NetworkFitnessPair {
+    public double fitness;
+    public Network network;
+    public string name;
+
+    public NetworkFitnessPair(double fitness, Network network) {
+        this.fitness = fitness;
+        this.network = network;
+    }
+
+    public NetworkFitnessPair(Network network) {
+        this.fitness = 0;
+        this.network = network;
+    }
+
+    public NetworkFitnessPair(NetworkFitnessPair network) {
+        this.fitness = network.fitness;
+        this.network = new Network(network.network, Network.CopyMethod.DEEP);
+    }
+}
+
+public class NetworkEvolver {
+    /*public static Network Aggregate(List<NetworkFitnessPair> weighted_networks) {
+
+        Network sample_network = weighted_networks.First().network;
+        Network final_network = new Network(sample_network, Network.CopyMethod.SHALLOW);
+
+        foreach (KeyValuePair<double, Network> weighted_network in weighted_networks) {
+            double weight = weighted_network.Key;
+            Network network = weighted_network.Value;
+            
+            for (int i = 0; i < network.input_layers; i++) {
+                for (int h = 0; h < network.hidden_layers; h++) {
+                    final_network.input_hidden_weights[i, h] += network.input_hidden_weights[i, h] * weight;
+                }
+            }
+
+            for (int h = 0; h < network.hidden_layers; h++) {
+                for (int o = 0; o < network.output_layers; o++) {
+                    final_network.hidden_output_weights[h, o] += network.hidden_output_weights[h, o] * weight;
+                }
+            }
+        }
+
+        for (int i = 0; i < final_network.input_layers; i++) {
+            for (int h = 0; h < final_network.hidden_layers; h++) {
+                final_network.input_hidden_weights[i, h] /= weighted_networks.Count;
+            }
+        }
+
+        for (int h = 0; h < final_network.hidden_layers; h++) {
+            for (int o = 0; o < final_network.output_layers; o++) {
+                final_network.hidden_output_weights[h, o] /= weighted_networks.Count;
+            }
+        }
+
+        return final_network;
+    }*/
+
+    public static List<NetworkFitnessPair> Cull(List<NetworkFitnessPair> fit_networks) {
+        double max_fitness = fit_networks.OrderByDescending(nf => nf.fitness).First().fitness;
+        return fit_networks.Where(nf => nf.fitness / max_fitness >= Random.value).ToList();
+    }
+
+    public static NetworkFitnessPair Couple(NetworkFitnessPair male, NetworkFitnessPair female) {
+        NetworkFitnessPair child = new NetworkFitnessPair(new Network(male.network, Network.CopyMethod.DEEP));
+
+        for (int i = 0; i < child.network.input_layers; i++) {
+            for (int h = 0; h < child.network.hidden_layers; h++) {
+                if (Random.value > 0.5)
+                    child.network.input_hidden_weights[i, h] = female.network.input_hidden_weights[i, h];
+            }
+        }
+
+        for (int h = 0; h < child.network.hidden_layers; h++) {
+            for (int o = 0; o < child.network.output_layers; o++) {
+                if (Random.value > 0.5)
+                    child.network.hidden_output_weights[h, o] = female.network.hidden_output_weights[h, o];
+            }
+        }
+
+        return child;
+    }
+
+    public static List<NetworkFitnessPair> Breed(List<NetworkFitnessPair> networks, int target_size, double variation) {
+        List<NetworkFitnessPair> new_networks = new List<NetworkFitnessPair>();
+        foreach (NetworkFitnessPair n in networks) {
+            new_networks.Add(new NetworkFitnessPair(n));
+        }
+        return FillGeneration(networks, new_networks, target_size, variation);
+    }
+
+    /*public static List<Network> NewGeneration(List<Network> networks, int target_size, double variation) {
+        return FillGeneration(networks, new List<Network>(), target_size, variation);
+    }*/
+
+    public static List<NetworkFitnessPair> FillGeneration(List<NetworkFitnessPair> old_networks, List<NetworkFitnessPair> new_networks, int target_size, double variation) {
+        while (new_networks.Count < target_size) {
+
+            int male_ind = Random.Range(0, old_networks.Count);
+            int female_ind = Random.Range(0, old_networks.Count);
+
+            NetworkFitnessPair child = Couple(old_networks[male_ind], old_networks[female_ind]);
+            if (variation != 0)
+                child.network.Mutate(variation);
+
+            new_networks.Add(child);
+        }
+
+        return new_networks;
+    }
+}
---
+++ SceneManager.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneManager : MonoBehaviour {
+
+
+    [Header("Simulation Speed")]
+    [Range(1, 100)]
+    public float Timescale = 1;
+
+    [Header("Evolution Settings")]
+    public NetworkProprietor EvolvingObject;
+    [Range(0, 1)]
+    public double MutationFactor = 0.02;
+    [Range(0, 1000)]
+    public int PopulationSize = 30;
+    [Range(0, 15)]
+    public float GracePeriod = 3;
+    [Range(0, 2)]
+    public double GraceExtensionMinimumDistance = 0.2;
+    [SerializeField]
+    public Transform StartingPosition;
+    public Canvas UI;
+    public Camera FollowingCamera;
+
+    List<NetworkFitnessPair> Population;
+    int subject_index = 0;
+    int generation = 1;
+    float last_fitness_increase;
+    double best_fitness;
+    double best_fitness_limiter;
+    NetworkProprietor simulating_object;
+    Vector3 DefaultOffset;
+    bool AdvancingGeneration = false;
+
+    void Start() {
+        EvolvingObject.Initialize();
+        Population = new List<NetworkFitnessPair>();
+        for (int i = 0; i < PopulationSize; i++) {
+            Population.Add(
+                new NetworkFitnessPair(new Network(EvolvingObject.network, Network.CopyMethod.RANDOM))
+            );
+        }
+        DefaultOffset = FollowingCamera.transform.position - StartingPosition.position;
+        CreateNewSubject();
+    }
+
+    void FixedUpdate() {
+
+        Time.timeScale = Timescale;
+
+        if (AdvancingGeneration)
+            return;
+
+        // Update UI Elements
+
+        Text GenCounter = UI.transform.FindChild("Generation").gameObject.GetComponent<Text>();
+        Text SubjectCounter = UI.transform.FindChild("Subject").gameObject.GetComponent<Text>();
+        Text FitnessTracker = UI.transform.FindChild("FitnessTracker").gameObject.GetComponent<Text>();
+        Text GraceTime = UI.transform.FindChild("GraceTime").gameObject.GetComponent<Text>();
+
+        GenCounter.text = $"Generation {generation}";
+        SubjectCounter.text = $"{subject_index + 1}/{Population.Count}";
+        FitnessTracker.text = $"{simulating_object.fitness:0.00} m [{best_fitness:0.00} m]";
+        GraceTime.text = $"{(last_fitness_increase + GracePeriod) - Time.fixedTime:0.00} s";
+
+        // Track Fitness
+
+        best_fitness = System.Math.Max(simulating_object.fitness, best_fitness);
+        if (simulating_object.fitness > best_fitness_limiter + GraceExtensionMinimumDistance) {
+            best_fitness_limiter = simulating_object.fitness;
+            last_fitness_increase = Time.fixedTime;
+        } else if (Time.fixedTime > last_fitness_increase + GracePeriod) {
+            if (subject_index >= Population.Count - 1) {
+                CloseGeneration();
+                generation += 1;
+                BeginNextGeneration();
+            } else {
+                EndCurrentSubject();
+                subject_index += 1;
+                CreateNewSubject();
+            }
+        }
+
+        // Update Camera
+
+        FollowingCamera.transform.position = simulating_object.transform.position + DefaultOffset;
+        FollowingCamera.transform.LookAt(simulating_object.transform);
+    }
+
+    public void EndCurrentSubject() {
+        Population[subject_index].fitness = simulating_object.fitness;
+        DestroyImmediate(simulating_object.gameObject);
+    }
+
+    public void CreateNewSubject() {
+        simulating_object = Instantiate(EvolvingObject);
+        simulating_object.transform.position = StartingPosition.position;
+        simulating_object.transform.rotation = StartingPosition.rotation;
+        simulating_object.network = Population[subject_index].network;
+        best_fitness = 0;
+        best_fitness_limiter = 0;
+        last_fitness_increase = Time.fixedTime;
+    }
+
+    public void CloseGeneration() {
+        AdvancingGeneration = true;
+        EndCurrentSubject();
+        int pop_size = Population.Count;
+        Debug.Log("\n\n\n            ===================    Generation Results:");
+        Population.ForEach(n => Debug.Log($"{n}: {n.fitness}"));
+        Population = NetworkEvolver.Cull(Population);
+        Debug.Log("            -------------------    Remaining specimens:");
+        Population.ForEach(n => Debug.Log($"{n}: {n.fitness}"));
+        Population = NetworkEvolver.Breed(Population, pop_size, MutationFactor);
+        Debug.Log("            -------------------    New Population:");
+        Population.ForEach(n => Debug.Log($"{n}: {n.fitness}"));
+    }
+
+    public void BeginNextGeneration() {
+        subject_index = 0;
+        CreateNewSubject();
+        AdvancingGeneration = false;
+    }
+}
+
+public abstract class NetworkProprietor : MonoBehaviour {
+
+    public abstract Network network { get; set; }
+    public abstract double fitness { get; set; }
+    public abstract void Initialize();
+}
